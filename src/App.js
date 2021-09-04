aed587652bed73e84fbee551590fc9976e7b9031
Sreelatha <sreelatha.vallabhaneni@gmail.com>
2021-09-04 18:30:17 +0200

    Created fetch function to fetch pokemon initial data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Header from './components/header';
 import './sass/main.scss';
-import './App.css';
+import { fetchPokemon } from './services/fetchPokemon';
 
 function App() {
+  const [next, setNext] = useState('');
+  const [previous, setPrevious] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    async function fetchData() {
+        try{
+      let response = await fetchPokemon(initialURL);
+      console.log(response);
+      setNext(response.next);
+      setPrevious(response.previous);
+      setLoading(false);
+        }
+        catch (error) {
+    console.log(error);
+  }
+    }
+    fetchData();
+  }, [])
   return (
     <div>
       <Header />
