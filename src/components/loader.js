59728457096b4ba724dbcc9b578821946208502c
Sreelatha <sreelatha.vallabhaneni@gmail.com>
2021-09-05 11:51:55 +0200

    created loader

---
+++ src/components/loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <img src='https://i.pinimg.com/originals/9a/3b/8f/9a3b8fa2ac8ebde3eec2079d8afd9ddc.gif' alt="loader"  />  
+        </div>
+    )
+}
+
+export default Loader;
