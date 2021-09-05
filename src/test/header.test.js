97845d79b822111210317a141c174bf3ac4f2987
Sreelatha <sreelatha.vallabhaneni@gmail.com>
2021-09-05 21:52:36 +0200

    installed enzyme and created unit test for header component

---
+++ src/test/header.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+import Header from '../components/header';
+import Logo from '../images/pokemon-logo.gif';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<Header />', () =>{
+    it('render logo', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.find("img").prop("src")).toEqual(Logo);
+    })
+})
