83b1ca5b72918bda745ef2937cd24df7a3f259b4
Sreelatha <sreelatha.vallabhaneni@gmail.com>
2021-09-05 13:22:38 +0200

    modified loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
     return (
         <div className="loader">
-            <img src='https://i.pinimg.com/originals/9a/3b/8f/9a3b8fa2ac8ebde3eec2079d8afd9ddc.gif' alt="loader"  />  
+            <img src='https://wpamelia.com/wp-content/uploads/2018/11/ezgif-2-6d0b072c3d3f.gif' alt="loader" width="100%"  />  
         </div>
     )
 }
